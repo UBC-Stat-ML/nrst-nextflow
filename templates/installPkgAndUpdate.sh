9178ef76d963aeb7e219bb45a8e7c40cacf52ee1
Miguel Biron-Lattes <miguelbl9@gmail.com>
2022-07-13 11:05:57 -0700

    testing

--- templates/installPkgAndUpdate.sh
+++ templates/installPkgAndUpdate.sh
@@ -2,8 +2,5 @@
 
 set -e
 
-#gitUser='UBC-Stat-ML'
-#gitRepoName='NRSTExp'
-#echo "using Pkg; Pkg.add(url=\"${gitRepoURL}\"); Pkg.update(); using ${gitRepoName}"
 julia -e "using Pkg; Pkg.add(url=\"git@github.com:${gitUser}/${gitRepoName}.git\"); Pkg.update(); using ${gitRepoName}"
 
