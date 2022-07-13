f1c52e1c23f48af0d98b82bc22ddf1d6fb6d3559
Miguel Biron-Lattes <miguelbl9@gmail.com>
2022-07-13 10:08:46 -0700

    try precompiling

--- templates/cloneRepoAndUpdate.sh
+++ templates/cloneRepoAndUpdate.sh
@@ -3,5 +3,5 @@
 set -e
 
 git clone git@github.com:${gitUser}/${gitRepoName}
-julia --project=${gitRepoName} -e "using Pkg; Pkg.update(); using ${gitRepoName}"
+julia --project=${gitRepoName} -e "using Pkg; Pkg.update(); Pkg.precompile(strict=true)"
 
