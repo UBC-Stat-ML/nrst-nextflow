ea0e82ceb08ea1f4061399582fec014bd32b7a70
Miguel Biron-Lattes <miguelbl9@gmail.com>
2022-08-09 11:11:04 -0700

    better jl env init

--- jl/set_up_env.jl
+++ jl/set_up_env.jl
@@ -1,10 +1,13 @@
 using Pkg
-Pkg.add(
-  [
-    Pkg.PackageSpec(url="git@github.com:miguelbiron/SplittableRandoms.jl.git"),
-    Pkg.PackageSpec(url="git@github.com:UBC-Stat-ML/NRST.jl.git"),
-    Pkg.PackageSpec(url="git@github.com:UBC-Stat-ML/NRSTExp.git")
-  ]
-)
-Pkg.precompile()
+
+if Base.find_package("NRSTExp") == nothing
+    Pkg.add(
+      [
+        Pkg.PackageSpec(url="git@github.com:miguelbiron/SplittableRandoms.jl.git"),
+        Pkg.PackageSpec(url="git@github.com:UBC-Stat-ML/NRST.jl.git"),
+        Pkg.PackageSpec(url="git@github.com:UBC-Stat-ML/NRSTExp.git")
+      ]
+    )
+end
+Pkg.update()
 
