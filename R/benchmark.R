f92b26515c338555100335029d511307460c970d
Miguel Biron-Lattes <miguelbl9@gmail.com>
2022-12-02 10:01:04 -0800

    benchmark plot

---
+++ R/benchmark.R
@@ -0,0 +1,30 @@
+library(dplyr)
+library(ggplot2)
+library(scales)
+library(tidyr)
+
+# load the latest consolidated file
+csvs = list.files(path       = file.path("..","deliverables"),
+                  pattern    = '^NRSTExp_\\d+.csv$',
+                  full.names = TRUE)
+dta  = read.csv(max(csvs))
+
+# parameters
+TE_min = 5e-4
+
+##############################################################################
+# plot: TE/(max V evals) for all models and real samplers
+##############################################################################
+
+dta %>% 
+  filter(cor == 0.90 & gam == 8.0 & fun == "mean" & !(proc %in% c("DTPerf", "DTAct"))) %>% 
+  mutate(tgt = TE/costpar) %>% 
+  ggplot(aes(x = proc, y=tgt)) +
+  geom_boxplot() +
+  scale_y_log10() +
+  facet_wrap(~ mod, scales="free_y", nrow = 1) +
+  theme_bw() +
+  labs(
+    x = "Sampler",
+    y = "Efficiency = TE/max(number of V(x) evals.)"
+  )
